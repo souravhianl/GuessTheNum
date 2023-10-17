e43cb21e7f130f9c816894bd136e5f88213fac6a
souravhianl <souravhianl97@gmail.com>
2023-10-17 15:05:51 +0530

    Final commit

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+let secretNumber = Math.trunc(Math.random() * 50) + 1;
+let score = 50;
 let highscore = 0;
 
 const displayMessage = function (message) {
